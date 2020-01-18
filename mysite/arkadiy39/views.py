30994ca0b52f4d43dbef56e11bc9e65696e1fdc3
AAA <gevork_ssh@mail.ru>
2020-01-18 15:50:02 +0200

    first part

--- mysite/arkadiy39/views.py
+++ mysite/arkadiy39/views.py
@@ -18,5 +18,5 @@ def books_by_rubric(request,rubric_id):
     books=Books.objects.filter(rubric=rubric_id)
     rubrics=Rubric.objects.all()
     current_rubric=Rubric.objects.get(pk=rubric_id)
-    context={'books':books,'rubrics':rubrics,'curent_rubric':current_rubric}
+    context={'books':books,'rubrics':rubrics,'current_rubric':current_rubric}
     return render(request,'arkadiy39/books_by_rubric.html',context)
